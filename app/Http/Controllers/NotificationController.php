d5d8811ec39ea5526c31f29aed746fddfd307b3c
nrcst <155081275+nrcst@users.noreply.github.com>
2024-10-02 14:59:36 +0700

    feat: add notification endpoints

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+    public function getNotifications($user_id)
+    {
+        $notifications = Notification::where('user_id', $user_id)->get();
+        return response()->json($notifications);
+    }
+
+    public function updateNotification(Request $request, $user_id, $notification_id)
+    {
+        $notification = Notification::where('user_id', $user_id)->findOrFail($notification_id);
+
+        $notification->update(array_merge($request->all(), ['is_read' => true]));
+
+        return response()->json($notification);
+    }
+}
